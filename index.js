25587649c9b079183a740b2bb4e893b8b9216210
Diego <diegoaugusto7@gmail.com>
2022-04-06 22:42:19 -0300

    3 commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
 
 // Define parâmetros (hostname e porta) e inicia o servidor
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, hostname, () => {
   console.log(` Servidor rodando http://${hostname}:${port}/`);
 });
